e7219a18836d252d2a3b60b319c60105f19f27a3
nabil <nabilmkadmi@gmail.com>
2023-12-11 12:24:21 -0800

    errors

--- errors.c
+++ errors.c
@@ -1,7 +1,7 @@
 #include "simple_shell.h"
 
 /**
- * handle_fork_error - Handles errors during the fork system call.
+ * handleforkerror - Handles errors during the fork system call.
  */
 void handle_fork_error(void)
 {
@@ -9,7 +9,7 @@ void handle_fork_error(void)
 }
 
 /**
- * handle_execvp_error - Handles errors during the execvp system call.
+ * handleexecvperror - Handles errors during the execvp system call.
  * @command: The command that caused the error.
  */
 void handle_execvp_error(const char *command)
@@ -19,7 +19,7 @@ void handle_execvp_error(const char *command)
 }
 
 /**
- * handle_waitpid_error - Handles errors during the waitpid system call.
+ * handlewaitpiderror - Handles errors during the waitpid system call.
  */
 void handle_waitpid_error(void)
 {
